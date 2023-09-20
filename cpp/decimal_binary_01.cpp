512d6e241515ac0f028663509d34247dc5c1b892
KCmean <kunalmanu2468@gmail.com>
2023-09-20 17:51:06 +0530

    day 1

--- cpp/decimal_binary_01.cpp
+++ cpp/decimal_binary_01.cpp
@@ -1,8 +1,31 @@
 #include <iostream>
+#include <math.h>
 using namespace std;
 
 int main()
 {
+    int n;
+    cin >> n;
+
+    int ans = 0;
+    int i = 0;
+    if ( n >= 0 ) {
+    while(n != 0 ){
+
+        int bit = n & 1;
+
+        ans = (bit * pow(10,i) + ans);
+
+        n = n >> 1;
+
+        i++;
+
+    }
+    } else {
+        
+    }
+
+    cout << "answer : " << ans << endl;
 
     return 0;
 }
