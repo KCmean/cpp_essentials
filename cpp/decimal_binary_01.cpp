d4ef778f085f2ab51a66c12a5bdbd72a553b96d4
KCmean <kunalmanu2468@gmail.com>
2023-09-21 13:32:47 +0530

    feat: 11:32

--- cpp/decimal_binary_01.cpp
+++ cpp/decimal_binary_01.cpp
@@ -9,23 +9,70 @@ int main()
 
     int ans = 0;
     int i = 0;
-    if ( n >= 0 ) {
-    while(n != 0 ){
 
-        int bit = n & 1;
+    // if (n >= 0)
+    // {
+    //     while (n != 0)
+    //     {
 
-        ans = (bit * pow(10,i) + ans);
+    //         int bit = n & 1;
+
+    //         ans = (bit * pow(10, i) + ans);
+
+    //         n = n >> 1;
+
+    //         i++;
+    //     }
+    // }
+    // else
+    // {
+        // n = n * -1;
+
+        // while (n != 0)
+        // {
+        //     int bit = n ^ 1;
+
+        //     ans = (bit * pow(10, i) + ans);
+
+        //     n = n >> 1;
+
+        //     i++;
+        // }
+
+        // int newAns = int(ans);
+        // newAns = ~newAns;
+        // newAns = newAns + 1;
+        // ans = float(newAns);
+
+        // n = -n;  Make n positive for further processing
+
+    //     while (n != 0) {
+    //         int bit = n & 1;
+    //         ans += (bit * pow(10, i));
+    //         n = n >> 1;
+    //         i++;
+    //     }
+
+    //     int newAns = int(ans);
+    //     newAns = ~newAns;
+    //     newAns = newAns | 1;
+    //     ans = float(newAns);    
+    
+
+    // }
+    // cout << "answer : " << ans << endl;
+    
+
+    while ( n!= 0) {
+        int bit = n &1; // yaha bit nahi digit nikal na hai lappu 
+
+        if(bit == 1){
+            ans = ans + pow(2,i);
+        }
 
         n = n >> 1;
-
         i++;
-
     }
-    } else {
-        
-    }
-
-    cout << "answer : " << ans << endl;
 
     return 0;
 }
